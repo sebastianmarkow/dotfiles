de98be3d08c647fc2ff992f5950d04c8253a4065
Sebastian Klatt <sebastian@markow.io>
2015-05-08 00:44:20 +0200

    Level up tmux

--- fish/functions/tmux.fish
+++ fish/functions/tmux.fish
@@ -1,3 +1,8 @@
 function tmux
-    command tmux attach-session -t $USER > /dev/null 2>&1; or command tmux -2 new-session -s $USER
+    switch (count $argv)
+        case 0
+            command tmux attach-session -t $USER > /dev/null 2>&1; or command tmux -2 new-session -s $USER
+        case '*'
+            command tmux $argv
+    end
 end
