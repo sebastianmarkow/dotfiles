adb1bff6e048f331ec0585dbd2a4680d8cedaf2c
Sebastian Klatt <sebastian@markow.io>
2015-06-03 20:51:18 +0200

    Modify fish git prompt

--- fish/config.fish
+++ fish/config.fish
@@ -13,6 +13,28 @@ set -x PATH $GOBIN $PATH
 set -x PATH /usr/local/Cellar/go/1.4.2/libexec/bin $PATH
 set -x HOMEBREW_NO_EMOJI 1
 
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch magenta
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate 'd'
+set __fish_git_prompt_char_stagedstate 's'
+set __fish_git_prompt_char_untrackedfiles 'u'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
 
 set -x LANG=en_US.UTF-8
 
