aa3372348387dff9bf0deb70a8367af484c9e3a5
Sebastian Klatt <sebastian@markow.io>
2015-06-11 18:21:08 +0200

    fish: Remove right prompt, add vi mode to ps1

--- fish/functions/fish_right_prompt.fish
+++
@@ -1,3 +0,0 @@
-function fish_right_prompt
-    echo -n -s (__fish_vi_prompt_cm)
-end
--- fish/functions/fish_vi_prompt.fish
+++ fish/functions/fish_vi_prompt.fish
@@ -7,8 +7,8 @@ function fish_vi_prompt --description 'Simple vi prompt'
 
     set -l pwd_info (prompt_pwd)
     set -l git_info (__fish_git_prompt)
-    # set -l vi_cm (__fish_vi_prompt_cm)
+    set -l vi_cm (__fish_vi_prompt_cm)
     set -l user_cm (__fish_user_prompt_cm)
 
-    echo -n -s $pwd_info $git_info ' ' $user_cm ' '
+    echo -n -s $pwd_info $git_info ' ' $vi_cm ' ' $user_cm ' '
 end
