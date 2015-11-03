4ff58522853f6568de1ee9ce91c189d4fa99ad2b
Sebastian Klatt <sebastian@markow.io>
2015-11-03 03:25:00 +0100

    brew: Sort formulas alphabetical, remove prefix

--- Homebrew.sh
+++ Homebrew.sh
@@ -8,62 +8,63 @@ TAPS=(
     homebrew/dupes
     homebrew/science
     homebrew/python
+    laurent22/massren
 )
 
 FORMULAS="vim --with-lua --with-luajit --without-ruby --without-perl \
-    homebrew/dupes/rsync \
-    homebrew/dupes/openssh \
-    homebrew/dupes/make \
-    homebrew/dupes/less \
-    homebrew/dupes/awk \
-    laurent22/massren/massren \
-    coreutils \
-    diffutils \
-    findutils \
-    moreutils \
-    gnu-tar \
-    gnu-sed \
-    fish \
-    bash \
-    tmux \
-    git \
-    hg \
-    bzr \
-    tig \
-    multitail \
     ag \
-    cloc \
-    watch \
-    calc \
-    entr \
-    nq \
-    tree \
-    since \
-    pv \
-    rename \
-    jq \
     autojump \
-    peco \
-    lftp \
-    nmap \
-    iftop \
-    wget \
-    curl \
-    mtr \
-    wrk \
-    tcpdump \
-    w3m \
-    gnuplot \
-    imagemagick \
-    ffmpeg \
+    awk \
+    bash \
+    bzr \
+    calc \
     cgdb \
     clang-format \
+    cloc \
+    coreutils \
+    curl \
+    diffutils \
+    entr \
+    ffmpeg \
+    findutils \
+    fish \
+    git \
+    gnu-sed \
+    gnu-tar \
+    gnuplot \
     go \
+    hg \
+    iftop \
+    imagemagick \
+    jq \
+    less \
+    lftp \
+    make \
+    massren \
+    moreutils \
+    mtr \
+    multitail \
+    nmap \
     node \
+    nq \
+    openssh \
+    peco \
+    pv \
     python \
-    homebrew/python/numpy \
-    homebrew/python/scipy \
-    homebrew/python/matplotlib"
+        numpy \
+        scipy \
+        matplotlib \
+    rename \
+    rsync \
+    since \
+    tcpdump \
+    tig \
+    tmux \
+    tree \
+    w3m \
+    watch \
+    wget \
+    wrk"
 
 brew_install() {
     [ -x "/usr/local/bin/brew" ] || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
