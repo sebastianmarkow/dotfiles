192708ca772034fec62f5ae93329a4649c718f7b
Sebastian Klatt <sebastian@markow.io>
2015-11-04 06:38:49 +0100

    fish: Add descriptive text to update function

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,4 +1,6 @@
 function update
+    echo -s (set_color red) "Updating OSX" (set_color normal)
     softwareupdate --install --all
+    echo -s (set_color red) "Updating Homebrew" (set_color normal)
     brew update --all; and brew upgrade --all
 end
