1e2124cac695884428d08eda093e487da82384e0
Sebastian Klatt <sebastian@markow.io>
2015-11-12 23:21:14 +0100

    fish: Add interactive mode to repo function

--- fish/functions/repo.fish
+++ fish/functions/repo.fish
@@ -1,15 +1,19 @@
 function repo --description 'Go to repository'
     if not type "ghq" > /dev/null 2>&1
         echo 'repo: error ghq not installed'
-        return
+        return 1
+    else if not type "fzf" > /dev/null 2>&1
+        echo "repo: error fzf not installed"
+        return 1
     end
     switch (count $argv)
         case 0
-            cd (ghq root)
+            ghq list | fzf | read fzf_last_select
+            if [ $fzf_last_select ]
+                cd (ghq list -e -p $fzf_last_select)
+            end
         case 1
-            cd (ghq list -p -e $argv[1])
-        case 2
-            echo $argv[1]
+            cd (ghq list -p -e $argv[1]) case 2 echo $argv[1]
             if test $argv[1] = '-d'
                 ghq list -p -e $argv[2] | xargs rm -rf
             end
