d8cf4d57c8a7a796801b27ce0ea59a934375d8eb
Sebastian Klatt <sebastian@markow.io>
2015-11-12 19:25:41 +0100

    fish: Add fish completion update

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -2,5 +2,9 @@ function update
     echo -s (set_color red) "Updating OSX" (set_color normal)
     softwareupdate --install --all
     echo -s (set_color red) "Updating Homebrew" (set_color normal)
-    brew update --all; and brew upgrade --all
+    brew update --all; and set -l brew_outdated (brew outdated); and brew upgrade --all
+    if test -n "$brew_outdated"
+        echo -s (set_color red) "Updating Fish completion" (set_color normal)
+        fish_update_completions
+    end
 end
