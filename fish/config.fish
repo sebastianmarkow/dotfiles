3c69db1aeedf19957d0908fbb3f0eab1849825a1
Sebastian Klatt <sebastian@markow.io>
2015-11-12 19:48:34 +0100

    fish: Clean config

--- fish/config.fish
+++ fish/config.fish
@@ -1,72 +1,57 @@
 # No, no, no!
 status --is-interactive; or exit 0
+set -e fish_greeting
 
-# Path
 set -x PATH /usr/local/bin /usr/local/sbin $PATH
-
 set -x TERM xterm-256color
-set -x LSCOLORS "gxfxdxcxbxexexhbhdacad";
-set -x LS_COLORS "di=36:ln=35:so=33:pi=32:ex=31:bd=34:cd=34:su=37;41:sg=37;43:tw=00;42:ow=00;43:"
-
-set -x LANG en_US.UTF-8
-set -x LC_CTYPE "en_US.UTF-8"
-set -x LC_MESSAGES "en_US.UTF-8"
-set -x LC_COLLATE C
-
-# Editor
 set -x EDITOR vim
 set -x VISUAL $EDITOR
 set -x PAGER less
 set -x LESS '--ignore-case --chop-long-lines --long-prompt --silent'
 set -x GREP_OPTIONS '--color=auto'
 set -x GREP_COLOR '3;33'
-
-# Golang
-set -x GOPATH $HOME/go
-set -x GOBIN $GOPATH/bin
-set -x PATH /usr/local/Cellar/go/1.4.2/libexec/bin $GOBIN $PATH
-
-# No greeting, thanks
-set -e fish_greeting
-
-# Alias
-alias nq "nq -q"
-
-# Homebrew
-set -x HOMEBREW_NO_EMOJI 1
+set -x LSCOLORS "gxfxdxcxbxexexhbhdacad";
+set -x LS_COLORS "di=36:ln=35:so=33:pi=32:ex=31:bd=34:cd=34:su=37;41:sg=37;43:tw=00;42:ow=00;43:"
+set -x LANG en_US.UTF-8
+set -x LC_CTYPE "en_US.UTF-8"
+set -x LC_MESSAGES "en_US.UTF-8"
+set -x LC_COLLATE C
 
 # Hooks
 [ -d $HOME/.local/share/fish/generated_completions ]; or fish_update_completions
 [ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
 
-# NQ
-set -x NQDIR $HOME/.cache/nq/
-
-# Node
+# Paths
 set -x PATH (brew --prefix node)/bin $PATH
+set -x HOMEBREW_NO_EMOJI 1
+set -x GOPATH $HOME/go
+set -x GOBIN $GOPATH/bin
+set -x NQDIR $HOME/.cache/nq/
+set -x PATH /usr/local/Cellar/go/1.4.2/libexec/bin $GOBIN $PATH
 
-# Python
+# Alias
+alias nq "nq -q"
+
+# Extensions
 eval (python2 -m virtualfish compat_aliases)
 
-# Git prompt
-set __fish_git_prompt_show_informative_status 1
-set __fish_git_prompt_showdirtystate 1
-set __fish_git_prompt_showuntrackedfiles 1
-set __fish_git_prompt_showcolorhints 1
+# Env
 
-# Color
 set fish_color_command yellow
 set fish_color_param cyan
 set fish_color_quote 'd26936'
 set fish_color_error red
 
-# Prompt
+set __fish_git_prompt_show_informative_status 1
+set __fish_git_prompt_showdirtystate 1
+set __fish_git_prompt_showuntrackedfiles 1
+set __fish_git_prompt_showcolorhints 1
+
 set __fish_user_prompt_string_root '#'
 set __fish_user_prompt_color_root red
 set __fish_user_prompt_string_default '$'
 set __fish_user_prompt_color_default normal
 
-# Prompt (vi)
 set __fish_vi_prompt_string_normal 'normal'
 set __fish_vi_prompt_string_insert 'insert'
 set __fish_vi_prompt_string_visual 'visual'
@@ -74,5 +59,5 @@ set __fish_vi_prompt_color_normal red
 set __fish_vi_prompt_color_insert cyan
 set __fish_vi_prompt_color_visual yellow
 
-# Start in vi mode
+# Vi mode
 fish_vi_mode
