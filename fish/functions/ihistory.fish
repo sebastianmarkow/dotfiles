95823b0294d2d88eb664c0aca70f936f88a4bc8c
Sebastian Klatt <sebastian@markow.io>
2015-11-12 23:21:45 +0100

    fish: Return status code 1 on error in ihistory

--- fish/functions/ihistory.fish
+++ fish/functions/ihistory.fish
@@ -1,7 +1,7 @@
 function ihistory --description "Interactive history"
     if not type "fzf" > /dev/null 2>&1
         echo "ihistory: error fzf not installed"
-        return
+        return 1
     end
     history | fzf --tiebreak=index | read fzf_last_select
     if [ $fzf_last_select ]
