a10f00bf0bea599a028fc5d6a660a89169f8e83e
Sebastian Klatt <sebastian@markow.io>
2015-11-15 05:09:25 +0100

    Use ctrl-p for fzf file completion globally

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
     bind \cr -M insert -m insert ihistory
     bind \cr -m insert ihistory
-    bind \cg -M insert -m insert __fzf_cmd
-    bind \cg -m insert __fzf_cmd
+    bind \cp -M insert -m insert __fzf_cmd
+    bind \cp -m insert __fzf_cmd
 end
