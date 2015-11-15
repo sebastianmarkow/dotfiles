ad38b11d81666ef63623cbf7e37d546c71a8e9fd
Sebastian Klatt <sebastian@markow.io>
2015-11-15 12:59:29 +0100

    fish: No alert in user controlled termination

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,10 +2,15 @@ function fish_prompt --description 'Compose fish prompt'
 
     set -l last_status $status
     set -l prompt_sign (__fish_prompt_sign)
-    set -l sign $prompt_sign
+    set -l sign
 
-    if test $last_status -ne 0
-        set sign (set_color red)$__fish_prompt_sign_error(set_color normal)
+    switch $last_status
+        case 0
+            set sign $prompt_sign
+        case 130
+            set sign $prompt_sign
+        case '*'
+            set sign (set_color red)$__fish_prompt_sign_error(set_color normal)
     end
 
     echo -n (prompt_pwd) $sign ''
