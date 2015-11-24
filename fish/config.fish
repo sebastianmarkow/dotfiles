b8834ee66d30fdbb22e853deb114b0243aa60219
Sebastian Klatt <sebastian@markow.io>
2015-11-24 03:08:50 +0100

    fish: Edit fzf colors

--- fish/config.fish
+++ fish/config.fish
@@ -25,7 +25,7 @@ set -x LC_COLLATE C
 
 # Env
 set -x HOMEBREW_NO_EMOJI 1
-set -x FZF_DEFAULT_OPTS '--color=16,header:11 --inline-info'
+set -x FZF_DEFAULT_OPTS '--color=16,header:13,info:5,pointer:3,marker:9,spinner:1,prompt:5,fg:7,hl:14,fg+:3,hl+:9 --inline-info --bind=shift-tab:toggle-down,tab:toggle-up'
 set -x FZF_DEFAULT_COMMAND 'ag -g ""'
 set -x YANKCM 'pbcopy make'
 
