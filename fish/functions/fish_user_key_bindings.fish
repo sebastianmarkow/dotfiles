4dc3e1a191e447a72c1d24d96df0cbac6273330c
Sebastian Klatt <sebastian@markow.io>
2015-11-26 03:49:25 +0100

    fish: Remove ctrl-p

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,4 @@
 function fish_user_key_bindings
     bind \cr -M insert -m insert ihistory
     bind \cr -m insert ihistory
-    bind \cp -M insert -m insert __fzf_cmd $HOME
-    bind \cp -m insert __fzf_cmd $HOME
 end
