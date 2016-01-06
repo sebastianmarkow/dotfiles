5cae23ee70d7812a6c01577420df0d3705068397
Sebastian Klatt <sebastian@markow.io>
2016-01-06 16:20:02 +0100

    fish: Add brew prune to update function

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -2,7 +2,7 @@ function update --description "Update system"
     echo -s (set_color red) "Updating OSX" (set_color normal)
     softwareupdate --install --all
     echo -s (set_color red) "Updating Homebrew" (set_color normal)
-    brew update --all; and set -l brew_outdated (brew outdated); and brew upgrade --all; and brew cleanup
+    brew update --all; and set -l brew_outdated (brew outdated); and brew upgrade --all; and brew cleanup; and brew prune
     if test -n "$brew_outdated"
         echo -s (set_color red) "Updating Fish completion" (set_color normal)
         fish_update_completions
