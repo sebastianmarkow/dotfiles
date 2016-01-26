13beee5665b760f0f9fa28d391e381c69dc93bb4
Sebastian Klatt <sebastian@markow.io>
2016-01-26 10:37:09 +0100

    fish: Refactor update

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,12 +1,13 @@
 function update --description "Update system"
-    echo -s (set_color red) "Updating OSX" (set_color normal)
+    echo -s (set_color red) "Update OSX" (set_color normal)
     softwareupdate --install --all
-    echo -s (set_color red) "Updating Homebrew" (set_color normal)
-    brew update --all; and set -l brew_outdated (brew outdated); and brew upgrade --all; and brew cleanup; and brew prune
-    if test -n "$brew_outdated"
-        echo -s (set_color red) "Updating Fish completion" (set_color normal)
+    echo -s (set_color red) "Update Homebrew" (set_color normal)
+    brew update; and set -l BREW_OUTDATED (brew outdated)
+    if test -n "$BREW_OUTDATED"
+        brew upgrade --all; and brew prune; and brew cleanup
+        echo -s (set_color red) "Update Completion" (set_color normal)
         fish_update_completions
     end
-    echo -s (set_color red) "Updating vim plugins" (set_color normal)
+    echo -s (set_color red) "Update Vim" (set_color normal)
     vim +PlugUpgrade +PlugClean! +PlugUpdate! +qall
 end
