e57968455034848cef0658bfbe4d59a5ed02e8a1
Sebastian Klatt <sebastian@markow.io>
2016-01-26 10:03:31 +0100

    fish: Do not force 256 color mode on tmux

--- fish/functions/tmux.fish
+++ fish/functions/tmux.fish
@@ -1,7 +1,7 @@
 function tmux --description 'Attach to tmux session'
     switch (count $argv)
         case 0
-            command tmux attach-session -t $USER > /dev/null 2>&1; or command tmux -2 new-session -s $USER
+            command tmux attach-session -t $USER > /dev/null 2>&1; or command tmux new-session -s $USER
         case '*'
             command tmux $argv
     end
