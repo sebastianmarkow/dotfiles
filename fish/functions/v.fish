636f45fbbb16f473723683dc8e8afac7a3c60ee4
Sebastian Klatt <sebastian@markow.io>
2016-02-25 20:31:35 +0100

    Obey xdg directories

--- fish/functions/v.fish
+++
@@ -1,3 +0,0 @@
-function v --description "Edit recently opened files in vim"
-    grep "^>" ~/.cache/vim/viminfo | cut -c3- | while read line; set line (string replace -r "~" $HOME $line); if [ -f $line ]; echo $line; end; end | fzf -m --header="vim recently opened" | xargs -o vim --
-end
