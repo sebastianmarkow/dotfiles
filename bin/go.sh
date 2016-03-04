20763ef3fea9735c19714636b92e04b97c3dc722
Sebastian Klatt <sebastian@markow.io>
2016-03-04 13:52:39 +0100

    go: Add more go tools

--- bin/go.sh
+++ bin/go.sh
@@ -5,19 +5,26 @@ set -e
 PATH=/usr/local/bin:$PATH
 
 TOOLS=(
+    "github.com/alecthomas/gometalinter"
+    "github.com/golang/lint/golint"
+    "github.com/lukehoban/go-find-references"
+    "github.com/lukehoban/go-outline"
+    "github.com/mailgun/godebug"
+    "github.com/motemen/gore"
+    "github.com/newhook/go-symbols"
+    "github.com/nsf/gocode"
+    "github.com/pranavraja/tldr"
+    "github.com/rogpeppe/godef"
+    "github.com/tpng/gopkgs"
     "golang.org/x/tools/cmd/benchcmp"
     "golang.org/x/tools/cmd/cover"
     "golang.org/x/tools/cmd/godoc"
     "golang.org/x/tools/cmd/goimports"
     "golang.org/x/tools/cmd/gorename"
+    "golang.org/x/tools/cmd/gorename"
     "golang.org/x/tools/cmd/stringer"
     "golang.org/x/tools/cmd/vet"
-    "github.com/rogpeppe/godef"
-    "github.com/nsf/gocode"
-    "github.com/alecthomas/gometalinter"
-    "github.com/mailgun/godebug"
-    "github.com/motemen/gore"
-    "github.com/pranavraja/tldr"
+    "sourcegraph.com/sqs/goreturns"
 )
 
 FORMULAS="go"
