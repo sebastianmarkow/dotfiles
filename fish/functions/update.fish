9d3928f5fd8155587aba00cd007bed82f5a18275
Sebastian Klatt <sebastian@markow.io>
2016-04-24 05:50:35 +0200

    fish: Fast check osx updates

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,6 +1,11 @@
 function update --description "Update system"
+
     echo -s (set_color red) "Update OSX" (set_color normal)
-    softwareupdate --install --all
+    set -l OSX_OUTDATED (softwareupdate --list --no-scan 2>&1 | grep "No new software available.")
+    if test -z "$OSX_OUTDATED"
+        softwareupdate --install --all
+    end
+
     echo -s (set_color red) "Update Homebrew" (set_color normal)
     brew update; and set -l BREW_OUTDATED (brew outdated)
     if test -n "$BREW_OUTDATED"
@@ -8,6 +13,8 @@ function update --description "Update system"
         echo -s (set_color red) "Update Completion" (set_color normal)
         fish_update_completions
     end
+
     echo -s (set_color red) "Update Neovim" (set_color normal)
     nvim +PlugUpgrade +PlugClean! +PlugUpdate! +UpdateRemotePlugins +qall
+
 end
