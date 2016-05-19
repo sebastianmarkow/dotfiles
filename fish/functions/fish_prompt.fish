43ee5c3d4a53c94ce181a53c2b7e5bfcd5418448
Sebastian Klatt <sebastian@markow.io>
2016-05-19 08:26:02 +0200

    fish: Delete temp var for prompt sign

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,7 +1,6 @@
 function fish_prompt --description 'Compose fish prompt'
 
     set -l last_status $status
-    set -l prompt_sign (__fish_prompt_sign)
 
     switch $last_status
         case 0
@@ -10,5 +9,5 @@ function fish_prompt --description 'Compose fish prompt'
             set last_status (set_color red) $__fish_prompt_sign_error (set_color normal) ' '
     end
 
-    echo -n -s $last_status (prompt_pwd) $prompt_sign ' '
+    echo -n -s $last_status (prompt_pwd) (__fish_prompt_sign) ' '
 end
