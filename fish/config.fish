6a7276b31bea2fd0c45208ab2d5bfd29f9ffd0c5
Sebastian Klatt <sebastian@markow.io>
2016-05-22 18:14:27 +0200

    fish: Strip color from fzf/ag

--- fish/config.fish
+++ fish/config.fish
@@ -28,7 +28,7 @@ set -x LC_COLLATE   'C'
 set -x HOMEBREW_NO_ANALYTICS 1
 set -x HOMEBREW_NO_EMOJI     1
 set -x FZF_DEFAULT_OPTS      '--color=16,header:13,info:5,pointer:3,marker:9,spinner:1,prompt:5,fg:7,hl:14,fg+:3,hl+:9 --inline-info --bind=shift-tab:toggle-down,tab:toggle-up'
-set -x FZF_DEFAULT_COMMAND   'ag -g ""'
+set -x FZF_DEFAULT_COMMAND   'ag -g "" --nocolor --nogroup'
 set -x YANKCM                'pbcopy make'
 set -x XZ_OPT                '-T0'
 
@@ -79,4 +79,4 @@ set __fish_mode_color_replace  red
 set __fish_mode_color_visual   yellow
 
 # Vi mode
-set -g fish_key_bindings fish_vi_key_bindings
+fish_vi_key_bindings
