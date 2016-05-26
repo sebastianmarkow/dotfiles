9a54bafeebd2bc54a7355a66b54524f05b3848f9
Sebastian Klatt (SK) <sebastian@markow.io>
2016-05-26 17:54:02 +0200

    fish: Remove log output on update

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -2,20 +2,15 @@ function update --description "Update system"
 
     set -l OSX_OUTDATED (softwareupdate --list --no-scan 2>&1 | grep "No new software available.")
     if test -z "$OSX_OUTDATED"
-        echo -s (set_color red) "Update OS" (set_color normal)
         softwareupdate --install --all
     end
 
-    echo -s (set_color red) "Update Homebrew" (set_color normal)
     brew update; and set -l BREW_OUTDATED (brew outdated)
     if test -n "$BREW_OUTDATED"
-        echo -s (set_color red) "Update Formulas" (set_color normal)
         brew upgrade --all; and brew prune; and brew cleanup -s
-        echo -s (set_color red) "Update Completion" (set_color normal)
         fish_update_completions
     end
 
-    echo -s (set_color red) "Update Neovim" (set_color normal)
     nvim "+let g:plug_window=''" +PlugUpgrade +PlugClean! +PlugUpdate! +UpdateRemotePlugins +qall
 
 end
