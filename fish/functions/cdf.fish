77012aefeb8c43a8f451b0ceeeb24be3d841924d
Sebastian Klatt <sebastian@markow.io>
2016-06-02 22:35:09 +0200

    fish: Add cdf, an interactive version of `cd`

---
+++ fish/functions/cdf.fish
@@ -0,0 +1,11 @@
+function cdf --description "Interactive cd"
+    if not type 'fzf' >/dev/null 2>&1
+        echo 'cdf: error fzf not installed'
+        exit 1
+    end
+
+    find * -type d | fzf --header="cd" --tiebreak=end,length | read -l fzf_last_select
+    if [ $fzf_last_select ]
+        cd $fzf_last_select
+    end
+end
