87ab4a563bd45255e879f2227c442f07b7931808
Sebastian Klatt <sebastian@markow.io>
2016-06-07 23:56:20 +0200

    rust: Add rustfmt

--- install/rust.sh
+++ install/rust.sh
@@ -2,12 +2,25 @@
 
 set -e
 
+CRATES=(
+    "rustfmt"
+)
+
 PATH=/usr/local/bin:$PATH
 
 FORMULAS="rust"
 
+cargo_install() {
+    for c in "${CRATES}"; do
+        printf "Install cargo crate %s" "$c"
+        cargo install --quiet "$c"
+        printf " ...done\n"
+    done
+}
+
 main() {
     brew install ${FORMULAS}
+    cargo_install
 }
 
 main
