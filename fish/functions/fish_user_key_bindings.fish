2048b19ab262ebd1f5e72e0ffff31b398d43207b
Sebastian Klatt <sebastian@markow.io>
2016-06-22 13:57:34 +0200

    fish: Add ctrl + j/k to iterate through history

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -3,4 +3,8 @@ function fish_user_key_bindings
     bind \cr -m insert historyf
     bind \cs -M insert -m insert insert_filepath
     bind \cs -m insert insert_filepath
+    bind \cj -M insert -m insert down-or-search
+    bind \cj -m insert down-or-search
+    bind \ck -M insert -m insert up-or-search
+    bind \ck -m insert up-or-search
 end
