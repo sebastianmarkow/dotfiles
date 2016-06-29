afc1fdec2a308500b804cbb373e6c7a8cff0cf00
Sebastian Klatt <sebastian@markow.io>
2016-06-29 06:44:05 +0200

    fish: Rename fzf headers

--- fish/functions/cdf.fish
+++ fish/functions/cdf.fish
@@ -4,7 +4,7 @@ function cdf --description "Interactive cd"
         exit 1
     end
 
-    find * -type d | fzf --header="cd" --tiebreak=end,length | read -l fzf_last_select
+    find * -type d | fzf --header="Change directory" --tiebreak=end,length | read -l fzf_last_select
     if [ $fzf_last_select ]
         cd $fzf_last_select
     end
--- fish/functions/insert_filepath.fish
+++ fish/functions/insert_filepath.fish
@@ -3,7 +3,7 @@ function insert_filepath --description 'Select/insert filepath into commandline'
         echo 'error: fzf not found'
         exit 1
     end
-    fzf --header="insert filepath" --preview="head {}" --preview-window=up:30% -m -q (commandline -t) | read -z -l fzf_last_select
+    fzf --header="Insert filepath" --preview="head -$LINES {}" -m -q (commandline -t) | read -z -l fzf_last_select
     if [ $fzf_last_select ]
         commandline -i (string split '\n' $fzf_last_select | string join ' ')
     end
