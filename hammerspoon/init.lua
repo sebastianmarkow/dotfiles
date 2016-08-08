1bc7f933406f8c0acc9f6942e4d60ebc445672d1
Sebastian Klatt <sebastian@markow.io>
2016-08-08 02:32:46 +0200

    Switch to hammerspoon from bitbar

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,7 @@
+-- Caffeinate replacement menubar
 local caffeine = hs.menubar.new()
+caffeine:setTitle("Caffeinate")
+caffeine:setTooltip("Caffeinate")
 
 function setCaffeineIcon(state)
     if state then
@@ -15,4 +18,53 @@ end
 if caffeine then
     caffeine:setClickCallback(caffeineClicked)
     setCaffeineIcon(hs.caffeinate.get("displayIdle"))
+end
+
+-- Homebrew menubar
+function getHomebrewOutdated()
+    brew = io.popen("/usr/local/bin/brew outdated -1 --verbose")
+    local outdated = {}
+    for formula in brew:lines() do
+        local full, name = formula, string.gmatch(formula, "%S+")[1]
+        table.insert(outdated, {title=full, fn=function() os.execute("/usr/local/bin/brew upgrade " .. name); updateHomebrewMenu() end})
+    end
+    brew:close()
+    return outdated
+end
+
+local homebrew = hs.menubar.new()
+homebrew:removeFromMenuBar()
+-- homebrew:setIcon("./icon/beer.pdf")
+homebrew:setTitle("Brew")
+homebrew:setTooltip("Homebrew")
+
+local defaultMenu = {
+    {title="Upgrade all", fn=function() os.execute("/usr/local/bin/brew upgrade --all"); updateHomebrewMenu() end},
+    {title="-"},
+}
+
+function updateHomebrewMenu()
+    local tableMenu = defaultMenu
+    local outdated = getHomebrewOutdated()
+
+    if (#outdated > 0) then
+        for item in ipairs(outdated) do
+            table.insert(tableMenu, item)
+        end
+        homebrew:returnToMenuBar()
+        homebrew:setMenu(tableMenu)
+    else
+        homebrew:removeFromMenuBar()
+        homebrew:setMenu(nil)
+    end
+end
+
+function cronHomebrew()
+    os.execute("/usr/local/bin/brew update")
+    updateHomebrewMenu()
+end
+
+if homebrew then
+    cronHomebrew()
+    homebrewTimer = hs.timer.new(3600, cronHomebrew)
 end
