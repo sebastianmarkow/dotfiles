4625e11f7d5054949bd81eaf33efc66e3191c4fc
Sebastian Klatt <sebastian@markow.io>
2016-08-08 18:06:16 +0200

    hammerspoon: Add battery safety while caffeinate

--- hammerspoon/modules/caffeinate.lua
+++ hammerspoon/modules/caffeinate.lua
@@ -1,6 +1,7 @@
 -- Caffeinate replacement menubar
 local Caffeinate = {
-    menubar = hs.menubar.new()
+    menubar = hs.menubar.new(),
+    batteryCap = 10.0,
 }
 
 function Caffeinate:setIcon(state)
@@ -15,8 +16,18 @@ function Caffeinate:toggle()
     self:setIcon(hs.caffeinate.toggle("displayIdle"))
 end
 
+function Caffeinate:batteryCallback()
+    if hs.battery.powerSource() == "Battery Power" then
+        if hs.battery.percentage <= self.batteryCap and hs.caffeinate.get("displayIdle") then
+            self:toggle()
+        end
+    end
+end
+
 if Caffeinate then
     Caffeinate.menubar:setTooltip("Caffeinate")
     Caffeinate.menubar:setClickCallback(function() Caffeinate:toggle() end)
     Caffeinate:setIcon(hs.caffeinate.get("displayIdle"))
+    watcher = hs.battery.watcher.new(function() Caffeinate:batteryCallback() end)
+    watcher:start()
 end
