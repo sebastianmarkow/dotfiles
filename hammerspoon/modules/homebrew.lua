7b7684f304c9d6ea8ee4dae9e336f9861e6d70c8
Sebastian Klatt <sebastian@markow.io>
2016-08-09 15:43:12 +0200

    hs: Refactor homebrew

--- hammerspoon/modules/homebrew.lua
+++ hammerspoon/modules/homebrew.lua
@@ -1,55 +1,48 @@
 -- Homebrew menubar
 local Homebrew = {
     menubar = hs.menubar.new(),
-    outdated = false,
-    items = {},
-    header = {
-        {title="Upgrade all", fn=function() hs.task.new("/usr/local/bin/brew", function(code, stdout, stderr) Homebrew:refresh() end, {"upgrade", "--all"}) end},
-        {title="-"}
-    }
+    items = {}
 }
 
 function Homebrew:loadOutdated()
-    self.items = {}
-    self.outdated = false
+    local items, outdated = {}, false
     local b = io.popen("/usr/local/bin/brew outdated -1 --quiet", "r")
     for formula in b:lines() do
-        table.insert(self.items, formula)
-        self.outdated = true
+        table.insert(items, formula)
+        outdated = true
     end
     b:close()
-end
 
-function Homebrew:loadMenu()
-    self.menubar:setMenu(nil)
-
-    if self.outdated then
-        local tableMenu = {}
-        tableMenu = self.header
-        for i, item in ipairs(self.items) do
-            table.insert(tableMenu, {title=item, fn=function() hs.task.new("/usr/local/bin/brew", function(code, stdout, stderr) Homebrew:refresh() end, {"upgrade", item}):start() end})
-        end
-        self.menubar:setMenu(tableMenu)
+    if outdated then
+        self.items = items
         self.menubar:returnToMenuBar()
     else
         self.menubar:removeFromMenuBar()
+        self.items = items
     end
-
 end
 
-function Homebrew:refresh()
-    self:loadOutdated()
-    self:loadMenu()
+function Homebrew:getMenu()
+    local tableMenu = {
+        {title="Upgrade all", fn=function() hs.task.new("/usr/local/bin/brew", function(code, stdout, stderr) Homebrew:loadOutdated() end, {"upgrade", "--all"}):start() end},
+        {title="-"},
+    }
+    for i, item in ipairs(self.items) do
+        table.insert(tableMenu, {title=item, fn=function() hs.task.new("/usr/local/bin/brew", function(code, stdout, stderr) Homebrew:loadOutdated() end, {"upgrade", item}):start() end})
+    end
+
+    return tableMenu
 end
 
 function Homebrew:update()
-    print("fetching homebrew")
-    hs.task.new("/usr/local/bin/brew", function(code, stdout, stderr) Homebrew:refresh() end, {"update"}):start()
+    print("Updating Homebrew")
+    hs.task.new("/usr/local/bin/brew", function(code, stdout, stderr) Homebrew:loadOutdated() end, {"update"}):start()
 end
 
 if Homebrew then
     Homebrew.menubar:removeFromMenuBar()
     Homebrew.menubar:setTooltip("Homebrew")
     Homebrew.menubar:setIcon("./assets/cask.pdf")
+    Homebrew.menubar:setMenu(function() return Homebrew:getMenu() end)
     Homebrew:update(); hs.timer.new(3600, function() Homebrew:update() end):start()
 end
