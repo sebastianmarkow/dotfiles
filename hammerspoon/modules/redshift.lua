19a80c27ccd75134b5539b48459d795b8df42f72
Sebastian Klatt <sebastian@markow.io>
2016-08-10 04:52:43 +0200

    hs: Minor formatting

--- hammerspoon/modules/redshift.lua
+++ hammerspoon/modules/redshift.lua
@@ -1,26 +1,26 @@
 -- Redshift menubar
 local Redshift = {
-    menubar = hs.menubar.new(),
-    active = false,
+    menubar     = hs.menubar.new(),
+    active      = false,
     temperature = 2700,
-    start = '20:00',
-    stop = '08:00',
-    fade = '2h',
-    inverted = false
+    start       = '20:00',
+    stop        = '08:00',
+    fade        = '2h',
+    inverted    = false,
 }
 
 function Redshift:on()
-        self.active = true
-        hs.settings.set('Redshift.active', self.active)
-        self.menubar:setIcon('./assets/moon.pdf')
-        hs.redshift.start(self.temperature, self.start, self.stop, self.fade, self.inverted)
+    self.active = true
+    hs.settings.set('Redshift.active', self.active)
+    self.menubar:setIcon('./assets/moon.pdf')
+    hs.redshift.start(self.temperature, self.start, self.stop, self.fade, self.inverted)
 end
 
 function Redshift:off()
-        self.active = false
-        hs.settings.set('Redshift.active', self.active)
-        self.menubar:setIcon('./assets/sun.pdf')
-        hs.redshift.stop()
+    self.active = false
+    hs.settings.set('Redshift.active', self.active)
+    self.menubar:setIcon('./assets/sun.pdf')
+    hs.redshift.stop()
 end
 
 function Redshift:toggle()
