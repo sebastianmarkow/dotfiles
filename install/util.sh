f28a8843903fb1ac97f8a9329d3203b4e0f3ac35
Sebastian Klatt <sebastian@markow.io>
2016-10-17 23:42:02 +0200

    util/cask: Add hammerspoon

--- install/util.sh
+++ install/util.sh
@@ -8,6 +8,7 @@ TAPS=(
     homebrew/dupes
     homebrew/science
     homebrew/services
+    caskroom/cask
 )
 
 EGGS=(
@@ -65,10 +66,16 @@ FORMULAS=(
     yank
 )
 
+CASKS=(
+    hammerspoon
+)
+
 h1 "utilities"
 h2 "brew taps"
 for t in "${TAPS[@]}"; do brew_tap "$t"; done
 h2 "brew formulas"
 for f in "${FORMULAS[@]}"; do brew_install "$f"; done
+h2 "brew casks"
+for f in "${CASKS[@]}"; do cask_install "$f"; done
 h2 "pip modules"
 for e in "${EGGS[@]}"; do pip_install "$e"; done
