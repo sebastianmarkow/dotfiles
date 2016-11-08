186dbff82b199291a23cbd32a0eb09cec5b12da3
Sebastian Klatt <sebastian@markow.io>
2016-11-08 19:38:13 +0100

    fish: Reintroduce ctrl+f for forward completion

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -3,4 +3,6 @@ function fish_user_key_bindings
     bind \cr -m insert historyf
     bind \cs -M insert -m insert insert_filepath
     bind \cs -m insert insert_filepath
+    bind \cf -M insert -m insert forward-char
+    bind \cf -m insert forward-char
 end
