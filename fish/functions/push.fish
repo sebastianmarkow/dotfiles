72f8446701febd8550ff092e59ce911683a39a5c
Sebastian Klatt <sebastian@markow.io>
2017-01-06 06:34:20 +0100

    fish: Add default message to push

--- fish/functions/push.fish
+++ fish/functions/push.fish
@@ -1,6 +1,12 @@
 function push --description "Send push notification via Pushover"
-    if not set -q PUSHOVER_TOKEN; or not set -q PUSHOVER_GROUP; or not test (count $argv) -eq 1; or test -z $argv[1]
+    if not set -q PUSHOVER_TOKEN; or not set -q PUSHOVER_GROUP
         return 1
+    end
+
+    set -l message "Knock Knock!"
+
+    if test (count $argv) -eq 1; and test -n $argv[1]
+        set message $argv[1]
     end
 
     set -l host (hostname -s)
@@ -9,6 +15,6 @@ function push --description "Send push notification via Pushover"
         -F "token=$PUSHOVER_TOKEN" \
         -F "user=$PUSHOVER_GROUP" \
         -F "title=from $host" \
-        -F "message=$argv[1]" \
+        -F "message=$message" \
         https://api.pushover.net/1/messages.json > /dev/null 2>&1
 end
