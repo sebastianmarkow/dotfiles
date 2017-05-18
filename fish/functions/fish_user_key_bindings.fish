6b77f7a67fdee31f856c0952156860db2299ecd2
Sebastian Klatt <sebastian@markow.io>
2017-05-18 18:51:11 +0200

    fish: Remap forward complete and filepath insert

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,8 @@
 function fish_user_key_bindings
     bind \cr -M insert -m insert historyf
     bind \cr -m insert historyf
-    bind \cs -M insert -m insert insert_filepath
-    bind \cs -m insert insert_filepath
-    bind \cf -M insert -m insert forward-char
-    bind \cf -m insert forward-char
+    bind \cf -M insert -m insert insert_filepath
+    bind \cf -m insert insert_filepath
+    bind \cs -M insert -m insert forward-char
+    bind \cs -m insert forward-char
 end
