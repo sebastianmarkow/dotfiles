fdabf1f5cd449c30e21a5d5d4b9c5a299ee90008
Sebastian Klatt <sebastian@markow.io>
2018-04-09 17:21:47 +0200

    fish: Add ctrl-p select & edit files in nvim

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -5,4 +5,6 @@ function fish_user_key_bindings
     bind \cf -m insert insert_filepath
     bind \cs -M insert -m insert forward-char
     bind \cs -m insert forward-char
+    bind \cp -M insert -m insert select_edit
+    bind \cp -m insert select_edit
 end
---
+++ fish/functions/select_edit.fish
@@ -0,0 +1,10 @@
+function select_edit --description 'Select files and open in editor'
+    if not type 'fzf' >/dev/null 2>&1
+        echo 'error: fzf not found'
+        exit 1
+    end
+    fzf --header="Edit files" --preview="head -$LINES {}" -m -q (commandline -t) | read -z -l fzf_last_select
+    if [ $fzf_last_select ]
+        string split '\n' $fzf_last_select | string join ' ' | xargs nvim -o
+    end
+end
