a6161bc32955e5c179d94826c2f32bef5ab75750
Sebastian Klatt <sebastian.klatt@trivago.com>
2018-12-31 16:33:56 +0100

    Fix cmd_timing

--- fish/functions/cmd_timing.fish
+++ fish/functions/cmd_timing.fish
@@ -4,15 +4,15 @@ function cmd_timing --description "Display execution time"
         set -l minutes ''
         set -l hours ''
         set -l days ''
-        set -l cmd_duration (math $CMD_DURATION/1000)
+        set -l cmd_duration (math --scale=0 $CMD_DURATION/1000)
         if test "$cmd_duration" -ge "$cmd_timing_limit"
-            set seconds (math $cmd_duration%68400%3600%60)'s'
+            set seconds (math --scale=0 $cmd_duration%68400%3600%60)'s'
             if test "$cmd_duration" -ge 60
-                set minutes (math $cmd_duration%68400%3600/60)'m'
+                set minutes (math --scale=0 $cmd_duration%68400%3600/60)'m'
                 if test "$cmd_duration" -ge 3600
-                    set hours (math $cmd_duration%68400/3600)'h'
+                    set hours (math --scale=0 $cmd_duration%68400/3600)'h'
                     if test "$cmd_duration" -ge 68400
-                        set days (math $cmd_duration/68400)'d'
+                        set days (math --scale=0 $cmd_duration/68400)'d'
                     end
                 end
             end
