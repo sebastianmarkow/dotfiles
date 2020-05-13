42d9337906195e1db290cc9fe4a987cd1c5b2258
Sebastian Klatt <sebastian@markow.io>
2020-05-13 09:00:24 +0200

    Add fonts

--- install/minimal.sh
+++ install/minimal.sh
@@ -4,6 +4,11 @@ PATH=/usr/local/bin:$PATH
 
 source "${BASH_SOURCE%/*}/lib.sh"
 
+TAPS=(
+    homebrew/cask
+    homebrew/cask-fonts
+)
+
 HEAD=(
 #    neovim/neovim/neovim
 )
@@ -40,6 +45,7 @@ FORMULAS=(
     reattach-to-user-namespace
     ripgrep
     rsync
+    ssh-copy-id
     tmux
     tree
     watch
@@ -54,11 +60,11 @@ CASKS=(
 h1 "minimal"
 h2 "brew taps"
 for t in "${TAPS[@]}"; do brew_tap "$t"; done
+h2 "brew casks"
+for f in "${CASKS[@]}"; do cask_install "$f"; done
 h2 "brew formulas (HEAD)"
 for h in "${HEAD[@]}"; do brew_install "$h" "--HEAD"; done
 h2 "brew formulas"
 for f in "${FORMULAS[@]}"; do brew_install "$f"; done
-h2 "brew casks"
-for f in "${CASKS[@]}"; do cask_install "$f"; done
 h2 "pip modules"
 for e in "${EGGS[@]}"; do pip_install "$e"; done
