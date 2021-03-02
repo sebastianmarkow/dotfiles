b537e20e41d4df9e4279493a0d3f20cef1ee7cf0
Sebastian Klatt <sebastian@markow.io>
2021-03-02 14:17:18 +0100

    Remove obsolete helm plugins
    - Remove obsolete taps

--- install/devops.sh
+++ install/devops.sh
@@ -4,12 +4,6 @@ PATH=/usr/local/bin:$PATH
 
 source "${BASH_SOURCE%/*}/lib.sh"
 
-TAPS=(
-    derailed/k9s
-    derailed/popeye
-    fluxcd/tap
-)
-
 FORMULAS=(
     act
     awscli
@@ -40,16 +34,8 @@ CASKS=(
     homebrew/cask/lens
 )
 
-HELM_PLUGINS=(
-    "helm plugin install https://github.com/instrumenta/helm-kubeval"
-)
-
 h1 "devops"
-h2 "brew taps"
-for t in "${TAPS[@]}"; do brew_tap "$t"; done
 h2 "brew casks"
 for f in "${CASKS[@]}"; do cask_install "$f"; done
 h2 "brew formulas"
 for f in "${FORMULAS[@]}"; do brew_install "$f"; done
-h2 "helm plugins"
-for f in "${HELM_PLUGINS[@]}"; do execute "$f"; done
