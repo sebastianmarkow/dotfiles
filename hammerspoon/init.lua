9345e558a97fdc834098e271ab52129f4a742ca6
Sebastian Klatt <sebastian.klatt@metro.digital>
2024-01-28 20:49:07 +0100

    style(hammerspoon): Format lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,23 +4,20 @@ if not hs.ipc.cliStatus() then
 end
 
 -- leader
-hs.settings.set('leader',      {'ctrl', 'alt'})
-hs.settings.set('leadercmd',   {'ctrl', 'alt', 'cmd'})
-hs.settings.set('leadershift', {'shift', 'ctrl', 'alt'})
+hs.settings.set("leader", { "ctrl", "alt" })
+hs.settings.set("leadercmd", { "ctrl", "alt", "cmd" })
+hs.settings.set("leadershift", { "shift", "ctrl", "alt" })
 
 -- hostname
 local hostname = hs.host.localizedName()
 
 -- host specific
-if hostname == 'tyrell' then
-
-elseif hostname == 'pris' then
-
+if hostname == "tyrell" then
+elseif hostname == "pris" then
 else -- any
-
 end
 
 -- loading modules
-require('./modules/caffeinate')
-require('./modules/grid')
-require('./modules/ms-teams')
+require("./modules/caffeinate")
+require("./modules/grid")
+require("./modules/ms-teams")
--- hammerspoon/modules/caffeinate.lua
+++ hammerspoon/modules/caffeinate.lua
@@ -1,55 +1,63 @@
 -- Caffeinate replacement menubar
 local Caffeinate = {
-    menubar    = hs.menubar.new(),
+    menubar = hs.menubar.new(),
     batteryCap = 10.0,
-    style      = {
-      strokeWidth  = 0,
-      strokeColor = { white = 1, alpha = 0 },
-      fillColor   = { white = 0, alpha = 0.25 },
-      textColor = { white = 1, alpha = 1 },
-      textFont  = ".AppleSystemUIFont",
-      textSize  = 27,
-      radius = 15,
-      atScreenEdge = 0,
-      fadeInDuration = 0.15,
-      fadeOutDuration = 0.15,
-      padding = nil,
-    }
+    style = {
+        strokeWidth = 0,
+        strokeColor = { white = 1, alpha = 0 },
+        fillColor = { white = 0, alpha = 0.25 },
+        textColor = { white = 1, alpha = 1 },
+        textFont = ".AppleSystemUIFont",
+        textSize = 27,
+        radius = 15,
+        atScreenEdge = 0,
+        fadeInDuration = 0.15,
+        fadeOutDuration = 0.15,
+        padding = nil,
+    },
 }
 
 function Caffeinate:setIcon(state)
     if state then
-        self.menubar:setIcon('./assets/caffeinate_active.pdf')
+        self.menubar:setIcon("./assets/caffeinate_active.pdf")
     else
-        self.menubar:setIcon('./assets/caffeinate_inactive.pdf')
+        self.menubar:setIcon("./assets/caffeinate_inactive.pdf")
     end
 end
 
 function Caffeinate:toggle(silent)
-    local state = hs.caffeinate.toggle('displayIdle')
+    local state = hs.caffeinate.toggle("displayIdle")
     self:setIcon(state)
     if not silent then
         if state then
-            hs.alert.show('Caffeinate on', self.style, nil, 1)
+            hs.alert.show("Caffeinate on", self.style, nil, 1)
         else
-            hs.alert.show('Caffeinate off', self.style, nil, 1)
+            hs.alert.show("Caffeinate off", self.style, nil, 1)
         end
     end
 end
 
 function Caffeinate:batteryCallback()
-    if hs.battery.powerSource() == 'Battery Power' then
-        if hs.battery.percentage() <= self.batteryCap and hs.caffeinate.get('displayIdle') then
+    if hs.battery.powerSource() == "Battery Power" then
+        if hs.battery.percentage() <= self.batteryCap and hs.caffeinate.get("displayIdle") then
             self:toggle()
-            hs.notify.show('Caffeinate disabled', '', 'Battery power below ' .. self.batteryCap .. '%')
+            hs.notify.show("Caffeinate disabled", "", "Battery power below " .. self.batteryCap .. "%")
         end
     end
 end
 
 if Caffeinate then
-    Caffeinate.menubar:setTooltip('Toggle Caffeinate')
-    Caffeinate.menubar:setClickCallback(function() Caffeinate:toggle() end)
-    Caffeinate:setIcon(hs.caffeinate.get('displayIdle'))
-    hs.battery.watcher.new(function() Caffeinate:batteryCallback() end):start()
-    hs.hotkey.bind(hs.settings.get('leader'), 'c', function() Caffeinate:toggle() end)
+    Caffeinate.menubar:setTooltip("Toggle Caffeinate")
+    Caffeinate.menubar:setClickCallback(function()
+        Caffeinate:toggle()
+    end)
+    Caffeinate:setIcon(hs.caffeinate.get("displayIdle"))
+    hs.battery.watcher
+        .new(function()
+            Caffeinate:batteryCallback()
+        end)
+        :start()
+    hs.hotkey.bind(hs.settings.get("leader"), "c", function()
+        Caffeinate:toggle()
+    end)
 end
