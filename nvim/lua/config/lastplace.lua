274ffa05616f4c9db0aeae7ea911fb4fb28de6dc
Sebastian Klatt <sebastian.klatt@metro.digital>
2024-09-13 20:12:20 +0200

    WIP

--- nvim/lua/config/lastplace.lua
+++ nvim/lua/config/lastplace.lua
@@ -1,6 +1,10 @@
 vim.api.nvim_create_autocmd('BufReadPost', {
   pattern = '*',
   callback = function()
+    if vim.bo.filetype == 'gitcommit' then
+      return
+    end
+
     local last_pos = vim.fn.line('\'"')
     if last_pos > 0 and last_pos <= vim.fn.line('$') then
       vim.cmd('normal! g`"')
