d80eeeb97fb00b9f1aae1d619b6f5d6d72a838b3
Sebastian Klatt <sebastian@markow.io>
2024-10-23 10:59:34 +0200

    WIP

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -351,4 +351,20 @@ return {
       end
     end,
   },
+  {
+    'alexghergh/nvim-tmux-navigation',
+    event = 'VeryLazy',
+    config = function()
+      local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+      nvim_tmux_nav.setup({
+        disable_when_zoomed = true,
+      })
+
+      vim.keymap.set('n', '<M-h>', nvim_tmux_nav.NvimTmuxNavigateLeft)
+      vim.keymap.set('n', '<M-j>', nvim_tmux_nav.NvimTmuxNavigateDown)
+      vim.keymap.set('n', '<M-k>', nvim_tmux_nav.NvimTmuxNavigateUp)
+      vim.keymap.set('n', '<M-l>', nvim_tmux_nav.NvimTmuxNavigateRight)
+    end,
+  },
 }
